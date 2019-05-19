6c32802216d21a3f0fd7b1a39b3ca95b12bd3630
arosa822 <h.rosa822@gmail.com>
2019-05-18 22:41:26 -0600

    add algortithm to sort through fields

--- patterns/StringToBinary/main.go
+++ patterns/StringToBinary/main.go
@@ -11,5 +11,48 @@ func stringToBinary(s string) (b string) {
 }
 
 func main() {
-	fmt.Println(stringToBinary("C"))
+
+	binaryRep := stringToBinary("C")
+
+	decoder := make(map[int32]int)
+	decoder[48] = 0
+	decoder[49] = 1
+	decoder[10] = 9
+	decoded := []int{}
+
+	// take in int32's and place in list of 0's and 1's
+	for n, j := range binaryRep {
+		fmt.Printf("%d: %v %T\n", n, j, j)
+		b, ok := decoder[j]
+		if ok {
+			//store b in string[]
+			decoded = append(decoded, b)
+		}
+
+	}
+
+	fmt.Println(decoded)
+	//var state int = 0
+	//var count int = 0
+	count := []int{}
+	val := []int{}
+	var tempCount int
+
+	for n, j := range decoded {
+
+		if n == 0 {
+			fmt.Println("first iteration")
+			tempCount = 1
+		} else if j != decoded[n-1] {
+			val = append(val, decoded[n-1])
+			count = append(count, tempCount-1)
+
+			tempCount = 1
+
+		}
+
+		tempCount++
+	}
+	fmt.Println(val)
+	fmt.Println(count)
 }
