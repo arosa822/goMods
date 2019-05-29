d8e4906520ca94fe58e054ce5740e5ee5f0ae10b
arosa822 <h.rosa822@gmail.com>
2019-05-28 20:55:49 -0600

    add methods for river generator

--- patterns/numberGames/parse_int/main.go
+++ patterns/numberGames/parse_int/main.go
@@ -4,9 +4,72 @@ package main
 // i.e. 708 converts to [7,0,8]
 // 4095 converts to [4,0,9,5]
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type river struct {
+	number, next int
+	byteArray    []byte
+	sum          byte
+}
+
+func (r *river) makeRiver() {
+	var rightMost, tempIntVar int
+	tempIntVar = r.number
+
+	// get the list of digits that make a number
+	for {
+		rightMost = tempIntVar % 10
+		// append right most int to begining of list
+		r.byteArray = append(r.byteArray, byte(rightMost))
+		// take off last processed number
+		tempIntVar /= 10
+		if tempIntVar == 0 {
+			break
+		}
+	}
+
+	// add up the numbers that make up intvar
+	for _, j := range r.byteArray {
+		r.sum += j
+	}
+	r.next = r.number + int(r.sum)
+
+}
 
 func main() {
-	num := 60
-	fmt.Println(num % 10)
+	var n river
+	n.number = 123
+	n.makeRiver()
+	fmt.Println(n.byteArray)
+	fmt.Println(n.sum)
+	fmt.Println(n.next)
+	/*
+		var intVar = 123
+		var rightMost, tempIntVar int
+		var byteArray []byte
+		tempIntVar = intVar
+
+		// get the list of digits that make a number
+		for {
+			rightMost = tempIntVar % 10
+			// append right most int to begining of list
+			byteArray = append(byteArray, byte(rightMost))
+			// take off last processed number
+			tempIntVar /= 10
+			if tempIntVar == 0 {
+				break
+			}
+		}
+
+		// add up the numbers that make up intvar
+		fmt.Println(byteArray)
+		var k byte = 0
+		for _, j := range byteArray {
+			k += j
+		}
+
+	*/
+
 }
---
+++ patterns/numberGames/scratchpad/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := 768
+	fmt.Println(n/10)
+	fmt.Println(n%10)
+}
