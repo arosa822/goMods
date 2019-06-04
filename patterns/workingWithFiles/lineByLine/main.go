5759b39df4f9df65991ee46877b8b72fd8ff5faa
arosa822 <h.rosa822@gmail.com>
2019-06-03 18:09:41 -0600

    add conditionals and cases for different scenarios

--- patterns/workingWithFiles/lineByLine/main.go
+++ patterns/workingWithFiles/lineByLine/main.go
@@ -15,6 +15,9 @@ func main() {
 
 	var state byte
 	state = 0
+	var status bool
+	fmt.Println(status)
+
 	fmt.Println(state)
 	if err != nil {
 		log.Fatal(err)
@@ -27,15 +30,34 @@ func main() {
 		fmt.Println(line)
 		if state == 0 && strings.Contains(line, "initializing") {
 			state = 1
+			status = false
+		}
+
+		// check for failure
+		if state == 1 && strings.Contains(line, "bomb") {
+			status = true
 		}
 
 		if state == 1 && strings.Contains(line, "down") {
 			s[0]++
+			state = 0
+			if status != true {
+				// reset status
+				status = true
+				s[1]++
+
+			} else {
+				s[2]++
+			}
 		}
+		// reset states status indicates a failure, state tells us if we are in
+		// the middle of a process
 
 	}
 
 	fmt.Printf("Number of cycles: %v\n", s[0])
+	fmt.Printf("Successfull Attempts %v\n", s[1])
+	fmt.Printf("Failed Attempts: %v\n", s[2])
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
